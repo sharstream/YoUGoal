3d97c3784548f0c5b0845c63205cd8eaf14ffdcb
David Perez <david.perez@us.abb.com>
2018-07-05 00:33:22 -0400

    react hot loader added to webpack

--- server/index.js
+++ server/index.js
@@ -5,13 +5,19 @@ import bodyParser from "body-parser";
 
 import webpack from "webpack";
 import webpackMiddleware from "webpack-dev-middleware";
+import webpackHotMIddleware from "webpack-hot-middleware";
 import webpackConfig from "../webpack.config.dev";
 
 const app = express();
-
+const compiler = webpack(webpackConfig);
 const port = process.env.PORT || 5000;
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+app.use(webpackMiddleware(compiler, {
+    hot: true,
+    publicPath: webpackConfig.output.publicPath,
+    noInfo: true
+}));
+app.use(webpackHotMIddleware(compiler));
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
