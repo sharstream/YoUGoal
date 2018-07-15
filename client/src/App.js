84a9659c047ae2799d42408452776a1bd82a1563
David Perez <david.perez@us.abb.com>
2018-07-15 12:21:54 -0400

    oktauth

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
@@ -13,6 +14,13 @@ import Carousel from "./components/Carousel";
 import { DropDownPlayer, DropDownTeam } from "./components/MenuDropDown";
 import { SignupPage as Signup, SigninPage as Signin } from "./components/SignPage";
 
+// import { ParentMenu as Menu } from "./Menu";
+// import { pushRotate as Menu } from "react-burger-menu";
+
+function onAuthRequired ({history}) {
+  history.push("/login");
+}
+
 const App = () => (
   <Router>
     <div className="container">
@@ -22,13 +30,26 @@ const App = () => (
       <div className="row">
         <Carousel />
       </div>
-      <Switch>
-        <Route exact path="/" component={Teams} />
-        <Route exact path="/teamsGet/:teamID" component={Players} />
-        <Route exact path="/player/:_id" component={Player} />
-        <Route component={NoMatch} />
-      </Switch>
-
+      <div className="row">
+        <Security issuer="https://dev-590113.oktapreview.com/oauth2/default"
+                  client_id="0oafq5xga3MOGlArd0h7"
+                  redirect_uri={window.location.origin + "/implicit/callback"}
+                  onAuthRequired={onAuthRequired}>
+          <Switch>
+            <Route exact={true} path="/signup" component={Signup} />
+            <Route exact path="/signin"
+              render={() => <Signin baseUrl="https://dev-590113.oktapreview.com" />}
+            />
+            <Route exact path="/implicit/callback" component={ImplicitCallback} />
+            <SecureRoute exact path="/dropdownplayer" component={DropDownPlayer} />
+            <SecureRoute exact path="/dropdownteam" component={DropDownTeam} />
+            <Route exact path="/" component={Teams} />
+            <Route exact path="/teamsGet/:teamID" component={Players} />
+            <Route exact path="/player/:_id" component={Player} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Security>
+      </div>
     </div>
   </Router>
 );
