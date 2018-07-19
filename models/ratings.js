2ffec713e7946333fa0e69b04b3089979940c635
David Perez <david.perez@us.abb.com>
2018-07-19 19:04:03 -0400

    client-ratings schema fields

--- models/ratings.js
+++ models/ratings.js
@@ -2,16 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ratingSchema = new Schema({
-    playerID: String,
-    ClientID: String,
-    playerTeamID: String,
-    overall: Number,
-    pace: Number,
-    dribbling: Number,
-    passing: Number,
-    shooting: Number,
-    defense: Number,
-    physicality: Number
+  playerID: String,
+  clientID: String,
+  playerTeamID: String,
+  username: String,
+  currentEmail: String,
+  overall: Number,
+  pace: Number,
+  dribbling: Number,
+  passing: Number,
+  shooting: Number,
+  defense: Number,
+  physicality: Number
 });
 
 const ratings = mongoose.model("ratings", ratingSchema);
