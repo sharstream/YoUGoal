350a5032587f287698d4bfb9b8684d6b93e7f31c
David Perez <david.perez@us.abb.com>
2018-09-25 11:14:14 -0400

    new mongo url

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI2, {
+  useNewUrlParser: true
+});
 
 // Start the API server
 app.listen(PORT, function() {
